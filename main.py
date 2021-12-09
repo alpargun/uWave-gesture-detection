d05a60fafe4d6dad7bf4918fa3a8f156eca9ba82
Alp Argun <alpargun@gmail.com>
2021-12-10 00:18:32 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+
+#%% Import statements
+
+
+
+#%% Load data
+
+# Top level:
+# $userIndex is the index of the participant from 1 to 8, and $dayIndex is the index
+# of the day from 1 to 7.
+
+# Inside each .rar file:
+# .txt files recording the time series of acceleration of each
+# gesture. The .txt files are named as [somePrefix]$gestureIndex-$repeatIndex.txt, where
+# $gestureIndex is the index of the gesture as in the 8-gesture vocabulary, and
+# $repeatIndex is the index of the repetition of the same gesture pattern from 1 to 10.
+
+
+
+#%% Preprocessing
+
+
+
+
+
+
+
+
+
+#%% Feature extraction
+
+
+#%% Training
+
+
+#%% Test
+ 
